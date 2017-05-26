52488db7bd84ea22768f4c11d8ca4b1bf040cfe9
jedsada <jt@20scoops.com>
2017-05-26 23:11:17 +0700

    first commit.

---
+++ app/src/main/java/com/pondthaitay/lifecycleactivity/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.pondthaitay.lifecycleactivity
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var TAG: String = javaClass.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onReStart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_next) {
+            startActivity(Intent(this, Main2Activity::class.java))
+        }
+    }
+}
